b2e075af72fab630da07543ddd0f310aec48b5f8
Corina Udrescu <corina.udrescu@gmail.com>
2025-04-15 23:17:13 +0300

    Update TypewriterEffect.jsx

--- src/TypewriterEffect.jsx
+++ src/TypewriterEffect.jsx
@@ -1,10 +1,47 @@
+import React, { useState, useEffect, useRef } from 'react';
+ 
+
+// Dan Abramov's useInterval hook
+// see https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+ 
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+ 
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+
 const TypewriterEffect = () => {
+  const [sentence, setSentence] = useState('');
+  const [cutoffIndex, setCutoffIndex] = useState(0);
+  const displayedText = sentence.substr(0, cutoffIndex);
+  const delay = 100;
+  const hasLettersLeft = cutoffIndex < sentence.length;
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = new FormData(e.target);
-    // TODO Display the text with typewriter effect
-    console.log(`The sentence to display is ${data.get("sentence")}`);
+    setSentence(data.get("sentence"));
+    setCutoffIndex(0);
   };
+
+  useInterval(() => {
+     setCutoffIndex(cutoffIndex + 1);
+  }, hasLettersLeft ? delay : null);
+
   return (
     <div>
       <form
@@ -22,6 +59,7 @@ const TypewriterEffect = () => {
         />
         <button type="submit">Display with typewriter effect</button>
       </form>
+      <h1>{displayedText}<h1/>
     </div>
   );
 };
